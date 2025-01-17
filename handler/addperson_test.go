76227565eddfcbffe9f91b5a7bb39d9ad06ff373
Afzal Ansari <afjalansari13998@gmail.com>
2025-01-17 14:21:45 +0530

    addperson code coverage implemented

--- handler/addperson_test.go
+++ handler/addperson_test.go
@@ -1,7 +1,46 @@
 package handler
 
-import "testing"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
 
-func TestAddPerson(t *testing.T) {
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/assert"
+)
+
+func TestAddOnePerson(t *testing.T) { // cmd to run this test func:- go test -run ^TestAddOnePerson$ ./...
+
+	// create mock request
+	requestBody := strings.NewReader(`{
+	 "id":"10","name":"Hardik","age":31,"address":"Hyderabad"
+	}`)
+
+	// create moke server
+	gin.SetMode(gin.TestMode)
+	router := gin.Default()
+	router.POST("/addperson", AddOnePerson)
+
+	//create http request
+	req, err := http.NewRequest("POST", "/addperson", requestBody)
+	if err != nil {
+		fmt.Println("Error while making http call:", err)
+	}
+	// req.Header.Set("Authorization", "Basic "+"dXNlcm5hbWU6cGFzc3dvcmQ=")
+
+	// record response
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	jsonBytes, _ := io.ReadAll(w.Body)
+	var result string
+	json.Unmarshal(jsonBytes, &result)
+	assert.Equal(t, ("One person added"), result)
 
 }
